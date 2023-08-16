5d5b071e2e09115b2d3383ec6913f98d7c181f4c
Olivier Hoareau <oha@greenberets.io>
2023-08-16 15:32:19 +0200

    feat: add accessDeniedError

---
+++ errors/accessDeniedError.go
@@ -0,0 +1,20 @@
+package errors
+
+type AccessDeniedError struct {
+	Err error
+}
+
+func (err AccessDeniedError) Error() string {
+	return err.Err.Error()
+}
+
+func (err AccessDeniedError) JsonResponse() JsonErrorResponse {
+	return JsonErrorResponse{
+		Status:     "error",
+		ErrorType:  "denied",
+		Message:    err.Error(),
+		Detail:     err.Err.Error(),
+		Code:       12993,
+		StatusCode: 403,
+	}
+}
